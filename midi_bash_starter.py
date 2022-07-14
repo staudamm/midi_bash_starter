40ebded7c7d6dff388bd10e5576c19b2cfebe259
staude <stoide@googlemail.com>
2022-07-14 16:02:47 +0200

    command line dialog for input selection

--- midi_bash_starter.py
+++ midi_bash_starter.py
@@ -1,5 +1,17 @@
 import mido
 import subprocess
 
+
+def select_input():
+    inputs = mido.get_input_names()
+    select_string = ""
+    for idx, inp in enumerate(inputs):
+        select_string += str(idx) + ": " + inp + "\n"
+    index = input("Select inputs by entering the corresponding number: \n" + select_string)
+    return inputs[int(index)]
+
+
 if __name__ == "__main__":
+    inp = select_input()
+    print("Selected: " + inp)
     subprocess.run(['/bin/bash', './default.sh'])
