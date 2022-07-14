cdea2d3601475b8ae81a5a478793257af678bbad
staude <stoide@googlemail.com>
2022-07-14 21:02:15 +0200

    Parse note from command line

--- midi_bash_starter.py
+++ midi_bash_starter.py
@@ -4,6 +4,7 @@ import asyncio
 
 midi_port = None
 channel = 0
+note_id = 0
 
 
 def select_input():
@@ -16,10 +17,11 @@ def select_input():
 
 
 def handle_input(message):
-    print(message)
     script_name = './default.sh'
-    if message.channel == channel:
+    if message.channel == channel and message.note == note_id:
         subprocess.run(['/bin/bash', script_name])
+    else:
+        print("received message", message)
 
 
 def connect_midi(port_name):
@@ -36,10 +38,16 @@ def select_channel():
     return 0 if user_input == "" else int(user_input)
 
 
+def select_note():
+    user_input = input('Enter note to listen to from 0 (default) t0 127')
+    return 0 if user_input == "" else int(user_input)
+
+
 if __name__ == "__main__":
     port_name = select_input()
     channel = select_channel()
-    print("Selected: " + port_name)
+    note_id = select_note()
+    print("Selected parameter: \n\tInput: {}\n\tChannel: {}\n\tNote: {}".format(port_name, channel, note_id))
     connect_midi(port_name)
     asyncio.run(loop())
 
--- sender.py
+++ sender.py
@@ -4,4 +4,4 @@ import mido
 if __name__ == "__main__":
     port_name = 'IAC Driver Bus 1'
     port = mido.open_output(port_name)
-    port.send(mido.Message('note_on', note=50, channel=2))
+    port.send(mido.Message('note_on', note=0, channel=0))
